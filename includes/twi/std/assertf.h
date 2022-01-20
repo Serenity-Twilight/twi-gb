a95f2639239c389f902212467d87225b4d188337
Serenity Twilight <Serenity_Twilight@protonmail.com>
2022-01-20 17:12:19 -0600

    Renamed macro assertf() to twi_assertf(), to make origin clearer and attempt to avoid unforeseen collisions.

--- includes/twi/std/assertf.h
+++ includes/twi/std/assertf.h
@@ -6,14 +6,14 @@
 #include <stdio.h>
 
 #ifndef TWI_NOASSERTF
-#	define assertf(condition, format, ...) \
+#	define twi_assertf(condition, format, ...) \
 		if (!(condition)) { \
 			fprintf(stderr, "assertion failure\n" __FILE__ ":%s:%ld\n(" \
 					#condition "): " format "\n", __func__, __LINE__, ##__VA_ARGS__); \
 			exit(EXIT_FAILURE); \
 		}
 #else
-#	define assertf(...) ((void)0)
+#	define twi_assertf(...) ((void)0)
 #endif
 
 #endif // TWI_ASSERTF_H
