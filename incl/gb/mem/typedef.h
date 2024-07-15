50ce1b19a8ad3305b0fc86762829cb1597609a52
Serenity Twilight <Serenity_Twilight@protonmail.com>
2024-07-15 06:02:09 -0500

    Made alignment of gb_mem members stricter to improve memcpy efficiency.

--- incl/gb/mem/typedef.h
+++ incl/gb/mem/typedef.h
@@ -1,5 +1,6 @@
 #ifndef GB_MEM_TYPEDEF_H
 #define GB_MEM_TYPEDEF_H
+#include <stdalign.h>
 
 //=======================================================================
 // doc struct gb_mem
@@ -7,8 +8,8 @@
 //=======================================================================
 // def struct gb_mem
 struct gb_mem {
+	alignas(max_align_t) uint8_t map[0x10000]; // 64 KiB
 	struct gb_pak* pak;
-	uint8_t map[0x10000]; // 64 KiB
 	uint8_t stat_int;
 	uint8_t ime;
 	uint8_t pad;
