43c4fefd9b492b798c7f6f623f51ef78d950b800
Serenity Twilight <Serenity_Twilight@protonmail.com>
2024-07-15 06:06:42 -0500

    Some comments describing member variable meanings.

--- incl/gb/mem/typedef.h
+++ incl/gb/mem/typedef.h
@@ -9,10 +9,10 @@
 // def struct gb_mem
 struct gb_mem {
 	alignas(max_align_t) uint8_t map[0x10000]; // 64 KiB
-	struct gb_pak* pak;
-	uint8_t stat_int;
-	uint8_t ime;
-	uint8_t pad;
+	struct gb_pak* pak; // Currently-inserted game pak.
+	uint8_t stat_int; // STAT interrupt line
+	uint8_t ime; // Interrupt Master Enable
+	uint8_t pad; // D-Pad + button input buffer
 }; // end struct gb_mem
 
 #endif // GB_MEM_TYPEDEF_H
