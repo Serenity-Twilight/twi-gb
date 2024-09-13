acc0a57abbf99b95922470da6e9335e62c64494f
Serenity Twilight <Serenity_Twilight@protonmail.com>
2024-09-13 15:04:54 -0500

    Changed mbc_ram_write() signature to accept abstract pointer to RAM map, instead of a gb_mem object.

--- incl/gb/pak/mbc/common.h
+++ incl/gb/pak/mbc/common.h
@@ -1,17 +1,16 @@
 #ifndef GB_PAK_MBC_COMMON_H
 #define GB_PAK_MBC_COMMON_H
 #include <stdint.h>
-#include "gb/mem.h"
-#include "gb/pak.h"
+#include "gb/pak/typedef.h"
 
 //=======================================================================
-// decl mbc_sram_write()
+// doc mbc_ram_write()
 // TODO: documentation
 //=======================================================================
 void
-mbc_sram_write(
-		struct gb_mem* restrict mem,
+mbc_ram_write(
 		struct gb_pak* restrict pak,
+		uint8_t* restrict ram_map,
 		uint16_t addr, uint8_t val);
 
 #endif // GB_PAK_MBC_COMMON_H
--- src/gb/pak/mbc/common.c
+++ src/gb/pak/mbc/common.c
@@ -1,30 +1,32 @@
 #include <assert.h>
 #include <stdint.h>
-#include "gb/mem/typedef.h"
-#include "gb/pak.h"
 #include "gb/pak/const.h"
 #include "gb/pak/mbc/common.h"
 #include "gb/pak/typedef.h"
 
 //=======================================================================
-// def mbc_sram_write()
+// def mbc_ram_write()
 void
-mbc_sram_write(
-		struct gb_mem* restrict mem,
+mbc_ram_write(
 		struct gb_pak* restrict pak,
+		uint8_t* restrict ram_map,
 		uint16_t addr, uint8_t val) {
-	assert(mem != NULL);
 	assert(pak != NULL);
 	assert(addr >= MEM_B_SRAM && addr < MEM_E_SRAM);
 
 	if (pak->ram_bank_count > 0) {
 		assert(pak->ram != NULL);
-		// Write to memory map's external RAM region:
-		mem->map[addr] = val;
-		// Write to pak's backing SRAM array:
-		addr -= MEM_B_SRAM;
-		((uint8_t*)pak->ram)[pak->ram_bank_curr * PAK_RAM_BANK_SIZE + addr] = val;
-		pak->dirty_ram = 1;
+		assert(pak->ram_bank_curr < pak->ram_bank_count);
+		if (pak->ram_enabled) {
+			addr -= MEM_B_SRAM;
+			// Update copy of RAM owned by memory map:
+			ram_map[addr] = val;
+			// Update RAM owned by pak:
+			((uint8_t*)pak->ram)[pak->ram_bank_curr * PAK_RAM_BANK_SIZE + addr] = val;
+			pak->dirty_ram = 1;
+		} // end if (pak->ram_enabled)
 	} // end if (pak->ram_bank_count > 0)
-} // end mbc_sram_write()
+} // end mbc_ram_write()
+
+// TODO: Move ROM & RAM swap functions here!
 
