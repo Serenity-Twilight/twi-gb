daefc378c0e25eeed81c5c757b8be918a8f0820c
Serenity Twilight <Serenity_Twilight@protonmail.com>
2024-09-24 06:16:30 -0500

    File commented out. Likely will be deleted prior to merge.

--- src/gb/pak/mbc.c
+++ src/gb/pak/mbc.c
@@ -1,7 +1,9 @@
-#include "gb/pak/mbc.h"
-#include "gb/pak/mbc/none.h"
-
-const struct mbc_write8_pair mbc_write8[] = {
-	{ .rom=mbc_write8_rom_none, .ram=mbc_write8_ram_none }, // NONE
-}; // end mbc_write8[]
-
+// TODO: This file has become redundant.
+//       Delete once certain it won't be reinstated.
+//#include "gb/pak/mbc.h"
+//#include "gb/pak/mbc/none.h"
+//
+//const struct mbc_write8_pair mbc_write8[] = {
+//	{ .rom=mbc_write8_rom_none, .ram=mbc_write8_ram_none }, // NONE
+//}; // end mbc_write8[]
+//
